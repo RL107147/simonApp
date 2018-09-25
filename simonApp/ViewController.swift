588172710b5eb10e1c7bf649c8b500a641fa527d
DHINGRA,GD GOLDY <rl107147@rlas-macair-14.rlas-116.org>
2018-09-25 12:26:15 -0500

    everything is completed

--- simonApp/ViewController.swift
+++ simonApp/ViewController.swift
@@ -2,8 +2,8 @@
 import UIKit
 import AVFoundation
 class ViewController: UIViewController {
-
-    @IBOutlet var colorViews: [UIView]!
+    
+    @IBOutlet var colorDisplays: [UIView]!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var colorsFrame: UIView!
@@ -17,41 +17,98 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playSound(fileName: "start")
         
     }
     @IBAction func onStartButtonTapped(_ sender: UIButton) {
+        if gameOver{
+            restart()
+            displayPattern()
+            gameOver = false
+            startButton.alpha = 0.0
+            messageLabel.text = ""
+        }
     }
+    
     @IBAction func onColorTapped(_ sender: UITapGestureRecognizer) {
+        if playerTurn {
+            for number in 0..<colorDisplays.count{
+                if colorDisplays[number].frame.contains(sender.location(in: colorsFrame)){
+                    if pattern[index] == number {
+                        flashColor(number: number)
+                        if index == pattern.count{
+                            index += 1
+                            if index == pattern.count{
+                                index = 0
+                                playerTurn = false
+                                messageLabel.text = ""
+                                addToPattern()
+                                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1){
+                                    self.displayPattern()
+                                }
+                            }
+                        }else{
+                            messageLabel.text = "Game Over"
+                            gameOver = true
+                            playSound(fileName: "lose")
+                            restart()
+                        }
+                    }
+                }
+            }
+        }
     }
+    
     func playSound(fileName: String){
         if let path = Bundle.main.path(forResource: fileName, ofType: "wav"){
-        let url = URL(fileURLWithPath: path)
-        do{
-            self.sound = try AVAudioPlayer(contentsOf: url)
-            self.sound?.play()
-        }
-        catch{
-            print("Can't find file")
+            let url = URL(fileURLWithPath: path)
+            do{
+                self.sound = try AVAudioPlayer(contentsOf: url)
+                self.sound?.play()
+            }
+            catch{
+                print("Can't find file")
+            }
         }
     }
-  
-
-
-}
+    
     func addToPattern() {
         pattern.append(Int(arc4random_uniform(4)))
     }
+    
     func restart(){
         pattern.removeAll()
         index = 0
         addToPattern()
         startButton.alpha = 1.0
     }
+    
     func flashColor(number: Int){
         self.playSound(fileName: String(number))
-        UIView.transition(with: colorsDisplays[number], duration: 0.2, options: .transitionCrossDissolve, animations: {
-            self.colorsDisplays[number].alpha
+        UIView.transition(with: colorDisplays[number], duration: 0.2, options: .transitionCrossDissolve, animations: {
+            self.colorDisplays[number].alpha = 1.0
+        }){ (true) in
+            UIView.transition(with: self.colorDisplays[number], duration: 0.2, options: .transitionCrossDissolve, animations: {
+                self.colorDisplays[number].alpha = 0.4
+            }, completion: nil)
+        }
     }
-
-}
+    
+    func displayPattern(){
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: (#selector(ViewController.nextColor)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func nextColor(){
+        if index < pattern.count {
+            flashColor(number: pattern[index])
+            index += 1
+        }else{
+            timer.invalidate()
+            index = 0
+            playerTurn = true
+            messageLabel.text = "your turn"
+        }
+    }
+    
+    
 }
---
+++ simonApp/ViewController2.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
